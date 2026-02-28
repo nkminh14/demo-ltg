e914463c2c282a830e9f07f9a98e594489c43713
nkminh14 <ngyenkhoaminh0104@gmail.com>
2026-02-28 20:45:05 +0700

    demo part 5

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+
 public class PlayerHealth : Health
 {
+    public GameObject gameOverCanvas;
+
     protected override void Die()
     {
         base.Die();
-        Debug.Log("Player died"); // Log riêng cho Player
+        Debug.Log("Player died");
+        if (gameOverCanvas != null)
+        {
+            gameOverCanvas.SetActive(true);
+        }
     }
 }
